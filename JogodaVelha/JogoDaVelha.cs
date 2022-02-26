e06339e01139413dc8cd469466ca2678eb96f06d
Fabio Zenatti Ferrenha <fabiozf3108@gmail.com>
2022-02-25 23:58:00 -0300

    Adicionar arquivos de projeto.

---
+++ JogodaVelha/JogoDaVelha.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JogodaVelha
+{
+    internal class JogoDaVelha
+    {
+        public string[,] Tabuleiro { get; set; }
+
+        public JogoDaVelha()
+        {
+            int posicao = 0;
+            string position;
+            Tabuleiro = new string[3, 3];
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    posicao++;
+                    position = ""+posicao;
+                    Tabuleiro[i, j] = position;
+                }
+            }
+        }
+
+        public void MostrarTabuleiro()
+        {
+            for(int i =0; i<3;i++)
+            {
+                for(int j = 0;j<3;j++)
+                {
+                    Console.Write("[{0}]",Tabuleiro[i,j]);
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public bool SituacaoJogo()
+        {
+            bool acabou = false;
+            if ((Tabuleiro[0,0] == Tabuleiro[1,1])&&(Tabuleiro[1, 1] == Tabuleiro[2,2]))
+                acabou = true;
+            if ((Tabuleiro[0, 2] == Tabuleiro[1, 1]) && (Tabuleiro[1, 1] == Tabuleiro[2, 0]))
+                acabou = true;
+            for(int i = 0;i<3;i++)
+            {
+                if((Tabuleiro[i, 0] == Tabuleiro[i, 1]) && (Tabuleiro[i, 1] == Tabuleiro[i, 2]))
+                    acabou = true;
+                if ((Tabuleiro[0, i] == Tabuleiro[1, i]) && (Tabuleiro[1, i] == Tabuleiro[2, i]))
+                    acabou = true;
+            }
+            return acabou;
+        }
+
+        public int Adicionar(int opc, string player, int play)
+        {
+            switch(opc)
+            {
+                case 1:
+                    if (Tabuleiro[0, 0] == "1")
+                        Tabuleiro[0, 0] = player;
+                    break;
+                case 2:
+                    if (Tabuleiro[0, 1] == "2")
+                        Tabuleiro[0, 1] = player;
+                    break;
+                case 3:
+                    if (Tabuleiro[0, 2] == "3")
+                        Tabuleiro[0, 2] = player;
+                    break;
+                case 4:
+                    if (Tabuleiro[1, 0] == "4")
+                        Tabuleiro[1, 0] = player;
+                    break;
+                case 5:
+                    if (Tabuleiro[1, 1] == "5")
+                        Tabuleiro[1, 1] = player;
+                    break;
+                case 6:
+                    if (Tabuleiro[1, 2] == "6")
+                        Tabuleiro[1, 2] = player;
+                    break;
+                case 7:
+                    if (Tabuleiro[2, 0] == "7")
+                        Tabuleiro[2, 0] = player;
+                    break;
+                case 8:
+                    if (Tabuleiro[2, 1] == "8")
+                        Tabuleiro[2, 1] = player;
+                    break;
+                case 9:
+                    if (Tabuleiro[2, 2] == "9")
+                        Tabuleiro[2, 2] = player;
+                    break;
+                default:
+                    break;
+            }
+            return play;
+        }
+
+        public bool VerificaPosicao(int posicao)
+        {
+            bool tablz = false;
+            switch (posicao)
+            {
+                case 1:
+                    if (Tabuleiro[0, 0] == "1")
+                        tablz = true;
+                    break;
+                case 2:
+                    if (Tabuleiro[0, 1] == "2")
+                        tablz = true;
+                    break;
+                case 3:
+                    if (Tabuleiro[0, 2] == "3")
+                        tablz = true;
+                    break;
+                case 4:
+                    if (Tabuleiro[1, 0] == "4")
+                        tablz = true;
+                    break;
+                case 5:
+                    if (Tabuleiro[1, 1] == "5")
+                        tablz = true;
+                    break;
+                case 6:
+                    if (Tabuleiro[1, 2] == "6")
+                        tablz = true;
+                    break;
+                case 7:
+                    if (Tabuleiro[2, 0] == "7")
+                        tablz = true;
+                    break;
+                case 8:
+                    if (Tabuleiro[2, 1] == "8")
+                        tablz = true;
+                    break;
+                case 9:
+                    if (Tabuleiro[2, 2] == "9")
+                        tablz = true;
+                    break;
+                default:
+                    tablz = false;
+                    break;
+            }
+            return tablz;
+        }
+    }
+}
---
+++ JogodaVelha/Program.cs
@@ -0,0 +1,136 @@
+﻿using System;
+
+namespace JogodaVelha
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int P1win = 0, P2win = 0, velha = 0; 
+            int opc;
+            int numerogame = 0;
+            string historico = "";
+            JogoDaVelha nJogo = null;
+            do
+            {
+                Console.ReadKey();
+                Console.Clear();
+                
+                opc = menu();
+                switch (opc)
+                {
+                    case 1:
+                        string player = "X";
+                        int play = 1, posicao;
+                        nJogo = new JogoDaVelha();
+                        Console.WriteLine("Player 1 = X\nPlayer 2 = O");
+                        for (int i = 0; i < 9; i++)
+                        {
+                            do
+                            {
+                                Console.Clear();
+                                nJogo.MostrarTabuleiro();
+                                Console.WriteLine("Player {0}", play);
+                                Console.Write("Digite a posição do seu [{0}]: ", player);
+                                if (int.TryParse(Console.ReadLine(), out int CanParse))
+                                    posicao = CanParse;
+                                else
+                                    posicao = -1;
+                                if (nJogo.VerificaPosicao(posicao) == false)
+                                {
+                                    Console.WriteLine("Posição invalida digite novamente!");
+                                    Console.ReadKey();
+                                }
+                            } while (nJogo.VerificaPosicao(posicao)==false);
+                            play = nJogo.Adicionar(posicao, player, play);
+                            if (nJogo.SituacaoJogo() != true)
+                            {
+                                if (play == 1)
+                                {
+                                    play = 2;
+                                    player = "O";
+                                }
+                                else
+                                {
+                                    play = 1;
+                                    player = "X";
+                                }
+                            }
+                            else
+                            {
+                                break;
+                            }
+                        }
+
+                        if (nJogo.SituacaoJogo() == true)
+                        {
+                            Console.Clear();
+                            Console.WriteLine("O player {0} venceu!", play);
+                            nJogo.MostrarTabuleiro();
+                            if (play == 1)
+                                P1win++;
+                            else
+                                P2win++;
+                        }
+                        else
+                        {
+                            Console.Clear();
+                            Console.WriteLine("Deu velha");
+                            nJogo.MostrarTabuleiro();
+                            velha++;
+                        }
+                        numerogame++;
+                        historico += "\nPartida" + numerogame + "\n";
+                        for(int i = 0; i < 3; i++)
+                        {
+                            for(int j = 0; j < 3; j++)
+                            {
+                                if((nJogo.Tabuleiro[i,j]=="1") || (nJogo.Tabuleiro[i, j] == "2") || (nJogo.Tabuleiro[i, j] == "3") || (nJogo.Tabuleiro[i, j] == "4") || (nJogo.Tabuleiro[i, j] == "5") || (nJogo.Tabuleiro[i, j] == "6") || (nJogo.Tabuleiro[i, j] == "7") || (nJogo.Tabuleiro[i, j] == "8") || (nJogo.Tabuleiro[i, j] == "9"))
+                                    historico += "[ ]";
+                                else
+                                    historico += "[" + nJogo.Tabuleiro[i,j] + "]";
+                            }
+                            historico += "\n";
+                        }
+                        
+                        break;
+                    case 2:
+                        Console.Clear();
+                        Console.WriteLine(">>>>PLACAR<<<<");
+                        Console.WriteLine("Player 1: {0}",P1win);
+                        Console.WriteLine("Player 2: {0}", P2win);
+                        Console.WriteLine("Velha: {0}", velha);
+                        break;
+                    case 3:
+                        Console.WriteLine(">>>>HISTORICO<<<<");
+                        Console.WriteLine(historico);
+                        break;
+                    case 0:
+                        Console.Clear();
+                        Console.WriteLine("Adeus ;-;");
+                        break;
+                    default:
+                        Console.Clear();
+                        Console.WriteLine("Opção Invalida!\nDigite novamente.");
+                        break;
+                }
+            }while(opc != 0);
+        }
+
+        static int menu()
+        {
+            int opc;
+            Console.WriteLine("=========JOGO DA VELHA=========");
+            Console.WriteLine("1-Novo Jogo\n2-Placar\n3-Historico\n0-Sair");
+            Console.Write("Opção: ");
+            if(int.TryParse(Console.ReadLine(),out int CanParse))
+            {
+                opc = CanParse;
+            }
+            else{
+                opc = -1;
+            }
+            return opc;
+        }
+    }
+}
